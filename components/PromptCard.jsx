f668e257254dbbe688186f63b3b6eb25ba2f04cd
Musonda Makena <Musondamakena@gmail.com>
2024-02-24 22:31:11 +0200

    feed page

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,15 +7,37 @@ import { useSession } from 'next-auth/react';
 
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete}) => {
 
-  console.log('this is the post data', post);
+  const [copied, setCopied] = useState("");
+
 
   return (
     <div className='prompt_card'>
       <div className='flex justify-between items-start gap-5'>
-        <div>
-      
+        <div className='flex-1 flex justify-start items-center gap-3 cursor-pointer'>
+          <Image
+          src={post.creator.image}
+          alt='user_img'
+          width={40}
+          height={40}
+          className='rounded-full object-contain'
+          />
+          <div className='flex flex-col'>
+            <h3 className='font-satoshi font-semibold text-gray-900'>{post.creator.username}</h3>
+            <p className='font-inter text-sm text-gray-500'>{post.creator.email}</p>
+          </div>
+        </div>
+        <div className='copy_btn' onClick={()=>{}}>
+          <Image
+          src={copied === post.prompt
+             ? '/assets/icons/tick.svg':
+            '/assets/icons/copy.svg'}
+            width={15}
+            height={15}
+          />
         </div>
       </div>
+      <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
+      <p className='font-inter text-sm blue_gradient cursor-pointer' onClick={()=> handleTagClick && handleTagClick(post.tag)}>{post.tag}</p>
       </div>
   )
 }
